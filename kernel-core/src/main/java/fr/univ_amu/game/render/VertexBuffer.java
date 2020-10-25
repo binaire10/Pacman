b76999cf3ef6bab2e501f22adcfbb8b015ac1705
Alexandre Wery <alexbwery@gmail.com>
2020-10-25 21:43:20 +0100

    add buffer to write function

--- kernel-core/src/main/java/fr/univ_amu/game/render/VertexBuffer.java
+++ kernel-core/src/main/java/fr/univ_amu/game/render/VertexBuffer.java
@@ -1,9 +1,15 @@
 package fr.univ_amu.game.render;
 
 import java.io.Closeable;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 
 public interface VertexBuffer extends Bindable, Closeable {
     void write(int[] data);
 
     void write(float[] data);
+
+    void write(IntBuffer data);
+
+    void write(FloatBuffer data);
 }
