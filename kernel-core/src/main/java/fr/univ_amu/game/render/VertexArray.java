79ca59c30e712d7f8e54bf72ac441712069f38e7
Alexandre Wery <alexbwery@gmail.com>
2020-10-25 11:10:28 +0100

    return component count

--- kernel-core/src/main/java/fr/univ_amu/game/render/VertexArray.java
+++ kernel-core/src/main/java/fr/univ_amu/game/render/VertexArray.java
@@ -4,4 +4,6 @@ public interface VertexArray extends Bindable {
     void setVertexBuffer(VertexBuffer vertexBuffer, BufferLayout bufferLayout);
 
     void setIndexBuffer(IndexBuffer indexBuffer);
+
+    int getComponentCount();
 }
