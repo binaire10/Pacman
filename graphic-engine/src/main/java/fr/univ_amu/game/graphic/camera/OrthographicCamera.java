d8fac147ead9b994894f30a926d86cc3df0734be
Alexandre Wery <alexbwery@gmail.com>
2020-10-26 12:50:03 +0100

    add ratio to orthographic camera

--- graphic-engine/src/main/java/fr/univ_amu/game/graphic/camera/OrthographicCamera.java
+++ graphic-engine/src/main/java/fr/univ_amu/game/graphic/camera/OrthographicCamera.java
@@ -1,22 +1,28 @@
 package fr.univ_amu.game.graphic.camera;
 
+import fr.univ_amu.game.math.Mat;
+
 public class OrthographicCamera implements Camera {
     private final float[] position = {0, 0, 0};
     private float angle = 0;
     private float zoom = 1;
+    private float ratio;
 
-    public OrthographicCamera() {
+    public OrthographicCamera(float ratio) {
+        this.ratio = ratio;
     }
 
-    public OrthographicCamera(float angle, float zoom) {
+    public OrthographicCamera(float ratio, float angle, float zoom) {
         this.angle = angle;
         this.zoom = zoom;
+        this.ratio = ratio;
     }
 
-    public OrthographicCamera(float[] position, float angle, float zoom) {
+    public OrthographicCamera(float ratio, float[] position, float angle, float zoom) {
         setPosition(position);
         this.angle = angle;
         this.zoom = zoom;
+        this.ratio = ratio;
     }
 
     @Override
@@ -42,12 +48,16 @@ public class OrthographicCamera implements Camera {
     public float[] getMatrix() {
         float cosA = (float) Math.cos(angle);
         float sinA = (float) Math.sin(angle);
-        return new float[]{
+        return Mat.dot_product(Mat.ortho(ratio, -ratio, -1, 1), new float[]{
                 cosA * zoom, -sinA, 0, position[0],
                 sinA, cosA * zoom, 0, position[1],
                 0, 0, zoom, position[2],
                 0, 0, 0, 1
-        };
+        }, 4);
+    }
+
+    public void setRatio(float ratio) {
+        this.ratio = ratio;
     }
 
     public float getZoom() {
