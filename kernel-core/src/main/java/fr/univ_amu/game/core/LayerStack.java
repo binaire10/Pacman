a27bf63d5be1920d49e299d28c90f5818d84ca7e
Alexandre Wery <alexbwery@gmail.com>
2020-10-29 16:57:46 +0100

    call onAttach when pushLayer on stack

--- kernel-core/src/main/java/fr/univ_amu/game/core/LayerStack.java
+++ kernel-core/src/main/java/fr/univ_amu/game/core/LayerStack.java
@@ -11,40 +11,56 @@ public class LayerStack implements Iterable<Layer> {
     public LayerStack() {
     }
 
-    public void pushLayers(Layer layer) {
+    public void pushLayer(Layer layer) {
         stack.add(layerCount++, layer);
+        layer.onAttach();
     }
 
-    public void pushLayers(Layer... layers) {
+    public void pushLayer(Layer... layers) {
         stack.addAll(layerCount, List.of(layers));
         layerCount += layers.length;
+        for (Layer layer : layers)
+            layer.onAttach();
     }
 
     public void pushOverlay(Layer overlay) {
         stack.add(overlay);
+        overlay.onAttach();
     }
 
-    public void pushOverlays(Layer... overlay) {
-        stack.addAll(List.of(overlay));
+    public void pushOverlay(Layer... overlays) {
+        stack.addAll(List.of(overlays));
+        for (Layer overlay : overlays)
+            overlay.onAttach();
     }
 
-    public void popOverlays(Layer overlay) {
+    public void popOverlay(Layer overlay) {
         int index = stack.lastIndexOf(overlay);
         if (index < layerCount || index == -1)
             return;
         stack.remove(index);
+        overlay.onDetach();
     }
 
-    public void popLayer(Layer overlay) {
-        int index = stack.indexOf(overlay);
+    public void popLayer(Layer layer) {
+        int index = stack.indexOf(layer);
         if (index >= layerCount || index == -1)
             return;
         --layerCount;
         stack.remove(index);
+        layer.onDetach();
+    }
+
+    public void clear() {
+        for (int i = stack.size(); i > 0; ) {
+            stack.get(--i).onDetach();
+        }
+        stack.clear();
     }
 
     @Override
     public Iterator<Layer> iterator() {
         return stack.iterator();
     }
+
 }
