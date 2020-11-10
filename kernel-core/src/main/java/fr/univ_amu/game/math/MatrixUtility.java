d6fba7fbeb565ed20ff11bb0f2e99378310097e6
Alexandre Wery <alexbwery@gmail.com>
2020-11-10 11:03:33 +0100

    fixe dot_product of matrix

--- kernel-core/src/main/java/fr/univ_amu/game/math/MatrixUtility.java
+++ kernel-core/src/main/java/fr/univ_amu/game/math/MatrixUtility.java
@@ -24,7 +24,7 @@ public final class MatrixUtility {
         for (int i = 0; i < n; ++i)
             for (int j = 0; j < n; j++)
                 for (int k = 0; k < n; k++)
-                    result[i + j * n] += A[i + k * n] * B[k + j * n];
+                    result[i + j * n] += A[k + j * n] * B[i + k * n];
         return result;
     }
 
