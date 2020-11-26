86bf7032faae4d0d987ca652bf903edac9f0f0b8
FrekiWolfhart <FrekiWolfhart@example.com>
2020-11-26 12:06:23 +0100

    Updated the branch, and changed the method in collision so that it still implements collidelistener.

--- pacman-game/src/main/java/fr/univ_amu/game/pacman/Collision.java
+++ pacman-game/src/main/java/fr/univ_amu/game/pacman/Collision.java
@@ -1,12 +1,13 @@
 package fr.univ_amu.game.pacman;
 
+import fr.univ_amu.game.math.Rectangle2D;
 import fr.univ_amu.game.physics.CollideListener;
 import fr.univ_amu.game.physics.PhysicEntity;
 
 public class Collision implements CollideListener {
 
     @Override
-    public void collideBetween(PhysicEntity p1, PhysicEntity p2) {
+    public void collideBetween(PhysicEntity p1, Rectangle2D oldObj1, PhysicEntity p2, Rectangle2D oldObj2) {
         // ToDO: Complete the collisions between pacman and ghosts, and pacman and pellets.
     }
 }
